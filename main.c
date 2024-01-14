e72bad7e84f24a960ae20f42b2b8bb149f8a3a54
Sabapty <saba.khmohammadi@gmail.com>
2024-01-14 16:06:58 +0330

    user and email

--- main.c
+++ main.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+void extractText(char* str, char userName[]) {
+    int start = -1;
+    int end = -1;
+    int len = strlen(str);
 
-int main()
-{
-    printf("HIIIIIII");
-    return 0;
+    for (int i = 0; i < len; i++) {
+        if (str[i] == '\"') {
+            if (start == -1) {
+                start = i + 1;
+            } else {
+                end = i;
+                break;
+            }
+        }
+    }
+
+
+        userName[end - start + 1];
+        strncpy(userName, str + start, end - start);
+        userName[end - start] = '\0';
 }
+int main() {
+    char userInput[1000];
+    char userName[1000];
+    char Gmail[1000];
+    while(1){
+    gets(userInput);
+
+    if (strncmp(userInput, "neogit config --user.name", 25) == 0) {
+        extractText(userInput, userName);
+
+    }
+    if (strncmp(userInput, "neogit config --user.email", 26) == 0){
+        extractText(userInput, Gmail);
+        FILE *file = fopen("user_info.txt", "w");
+        file = fopen("user_info.txt", "w");
+        if (file != NULL) {
+        fprintf(file, "UserName: %s\n", userName);
+        fprintf(file, "UserGmail: %s\n", Gmail);
+        fclose(file);
+        printf("User info saved successfully.\n");
+    } break;
+}
+
+    }
+
+    return 0;}
